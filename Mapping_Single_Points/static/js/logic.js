e79245eca1882a80ca13ad92d203b227a38fea21
James LaBouliere <jameslabouliere@Jamess-MacBook-Air.local>
2022-07-02 20:57:51 -0500

    Los Angeles map with yellow circle

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+// City markers
+cities = [];
+cities.push({ coord: [40.7648, -73.9808], name: "New York" });
+cities.push({ coord: [41.881832, -87.623177], name: "Chicago" });
+cities.push({ coord: [29.749907, -95.358421], name: "Houston" });
+cities.push({ coord: [34.052235, -118.243683], name: "L.A." });
+cities.push({
+    coord: [41.257160, -95.995102],
+    name: "Omaha"
+});
+// Add code to create a marker for each city below and add it to the map
+cities.forEach(city => {
+    L.marker(city.coord, {
+        draggable: false,
+    }).addTo(map).bindPopup(city.name + " is at " + city.coord)
+});
